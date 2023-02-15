4a4d62c3b6c98dd2e6abef5fa0a44e3418475499
Heli-DL <heli.delorme@outlook.com>
2023-02-15 20:31:33 +0200

    muutettu convertAmount funktiota

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput, Button, Image, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 
 export default function App() {
   const [amount, setAmount] = useState('');
-  const [currencies, setCurrencies] = useState([]);
+  const [rates, setRates] = useState({});
   const [selectedCurrency, setSelectedCurrency] = useState();
   const [result, setResult] = useState('');
 
@@ -19,33 +19,30 @@ export default function App() {
   };
   
   useEffect (() => {
-    fetch(`https://api.apilayer.com/exchangerates_data/latest?base=EUR`, requestOptions)
+    fetch(`https://api.apilayer.com/exchangerates_data/latest`, requestOptions)
     .then(response => response.json())
     .then(result => {
-      setCurrencies(Object.keys(result.rates));
+      console.log(result.rates);
+      setRates(result.rates);
     })
     .catch(error => console.log('error', error));
   }, []);
 
   const convertAmount = () => {
-    fetch(`https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=${selectedCurrency}&amount=${amount}`, requestOptions)
-    .then(response => response.json())
-    .then(data => {
-      setResult(data.result.toFixed(2));
-      console.log(data);
-    })
-    .catch(error => console.log('error', error));
+    const eur = Number(amount) / rates[selectedCurrency];
+    setResult(`${eur.toFixed(2)}€`)
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.box}>
         <Image source={require('./assets/euro-logo.jpg')} style={{width: 150, height: 150}}/>
-        <Text style={{fontSize: 22, fontWeight: 'bold', marginVertical: 20}}>{result} €</Text>
+        <Text style={{fontSize: 22, fontWeight: 'bold', marginVertical: 20}}>{result}</Text>
         <View style={styles.inputs}>
           <TextInput
             style={{fontSize: 18, width: 100, height: 40, borderBottomColor: 'grey', borderBottomWidth: 1, marginBottom: 5}}
-            placeholder='amount'
+            placeholder={'amount'}
+            keyboardType= 'numeric'
             value={amount}
             onChangeText={text => setAmount(text)}
           />
@@ -53,11 +50,11 @@ export default function App() {
             style={styles.picker}
             selectedValue={selectedCurrency}
             onValueChange={(itemValue, itemIndex) =>{
+              console.log(itemValue, itemIndex);
               setSelectedCurrency(itemValue);
             }}>
-            {currencies.map((item, index) => {
-              return (< Picker.Item label={item} value={item} key={index} />);
-            })}   
+            {Object.keys(rates).map(key =>
+              (< Picker.Item label={key} value={key} key={key} />))}   
           </Picker>
         </View>
       <Button title="Convert" onPress={convertAmount} />
